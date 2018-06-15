11ab970c30309b52d0814440ded04bdc2360f34c
mikelinden1 <mike@mikelinden.com>
2018-06-15 15:19:46 -0400

    added new components

---
+++ crayon-app/src/components/field-multi/index.js
@@ -0,0 +1,36 @@
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import getModuleReduxProp from 'utils/get-module-redux-prop';
+
+import { validateOnChange } from 'redux/actions/items';
+import { setItemProp } from 'redux/actions/modal-item-props';
+import { fetchDatasource } from 'redux/actions/data-sources';
+import { showMediaPicker } from 'redux/actions/media-picker';
+
+import { getPropValue } from 'redux/selectors/get-prop-value';
+import { getPropValidationErrors } from 'redux/selectors/get-prop-validation-errors';
+
+import FieldMulti from './field-multi';
+
+function mapStateToProps(state, ownProps) {
+    return {
+        ...ownProps,
+        value: getPropValue(state, ownProps),
+        datasources: getModuleReduxProp(state, 'datasources'),
+        validationErrors: getPropValidationErrors(state, ownProps)
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            setItemProp,
+            fetchDatasource,
+            validateOnChange,
+            showMediaPicker
+        }, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(FieldMulti);
---
+++ crayon-app/src/components/field-single/field-single.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Field from 'components/field';
+
+export default class FieldSingle extends React.PureComponent {
+    static propTypes = {
+        type: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        helpText: PropTypes.string,
+        validationErrors: PropTypes.array,
+        disabled: PropTypes.bool,
+        actions: PropTypes.shape({
+            setItemProp: PropTypes.func.isRequired,
+            validateOnChange: PropTypes.func.isRequired,
+            showMediaPicker: PropTypes.func.isRequired
+        }).isRequired
+    };
+
+    getField() {
+        const { name, validationErrors, actions: { setItemProp, validateOnChange, showMediaPicker } } = this.props;
+
+        const fieldProps = {...this.props};
+
+        fieldProps.onChange = (val) => {
+            setItemProp(name, val);
+
+            if (validationErrors.length) {
+                validateOnChange();
+            }
+        };
+
+        fieldProps.showMediaPicker = () => {
+            showMediaPicker(name);
+        };
+
+        return <Field {...fieldProps} />;
+    }
+
+    render() {
+        const { label, type, helpText: help, validationErrors } = this.props;
+
+        let field = this.getField();
+
+        if (type === 'checkbox') {
+            return field;
+        }
+
+        const fieldClasses = ['form-group'];
+
+        let errorText = '';
+        if (validationErrors.length) {
+            fieldClasses.push('has-error');
+            const errorTextP = validationErrors.map((e, i) => <p key={`errormsg-${e.itemName}-${i}`}>{e.msg}</p>);
+
+            errorText = <div className="invalid-feedback">{errorTextP}</div>;
+        }
+
+        const helpText = help ? <p><small><em>{help}</em></small></p> : null;
+
+        return (
+            <div className={fieldClasses.join(' ')}>
+                <label className="control-label">{label}</label>
+                {field}
+                {helpText}
+                {errorText}
+            </div>
+        );
+    }
+}

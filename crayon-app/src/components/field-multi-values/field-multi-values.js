107e3d8d2a9632d76efc40ced9e513a6d2c7aedc
mikelinden1 <mike@mikelinden.com>
2018-06-18 14:15:59 -0400

    added item props to field multi values

--- crayon-app/src/components/field-multi-values/field-multi-values.js
+++ crayon-app/src/components/field-multi-values/field-multi-values.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import FieldMultiValueWrapper from './field-multi-values-wrapper';
 
 export default class FieldMultiValue extends React.PureComponent {
+    static propTypes = {
+        name: PropTypes.string.isRequired,
+        allValue: PropTypes.oneOfType([
+            PropTypes.array,
+            PropTypes.string
+        ]).isRequired,
+        actions: PropTypes.shape({
+            multiItemSortEnd: PropTypes.func.isRequired
+        }).isRequired
+    };
+
     onSortEnd(e) {
         const { name, allValue, actions: { multiItemSortEnd } } = this.props;
 

8d0b1c9853357c96f0d45b858c963b98bef6f560
mikelinden1 <mike@mikelinden.com>
2018-07-08 21:14:50 -0400

    removed skift domain from setCookie

--- src/utils/set-cookie.js
+++ src/utils/set-cookie.js
@@ -3,8 +3,5 @@ export function setCookie(name, val, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     const expires = 'expires=' + d.toUTCString();
 
-    const host = window.location.hostname;
-    const domain = host === 'localhost' ? 'localhost' : host.indexOf('skift.com') !== -1 ? '.skift.com' : '.wpengine.com';
-
-    document.cookie = name + '=' + val + ';' + expires + ';path=/;domain=' + domain;
+    document.cookie = name + '=' + val + ';' + expires + ';';
 };

263fdb26c832bc9ff3626bc961b6a14e9fbc4345
mikelinden1 <mike@mikelinden.com>
2018-07-16 16:38:41 -0400

    fixed items per page values to be numbers

--- src/components/items-per-page/items-per-page.js
+++ src/components/items-per-page/items-per-page.js
@@ -20,11 +20,11 @@ export default class Paginator extends React.PureComponent {
         return (
             <div style={{margin: '20px 0', textAlign: 'center'}}>
                 <select value={itemsPerPage} onChange={(e) => setItemsPerPage(e.target.value)}>
-                    <option value="6">6 {itemNamePlural}</option>
-                    <option value="12">12 {itemNamePlural}</option>
-                    <option value="24">24 {itemNamePlural}</option>
-                    <option value="48">48 {itemNamePlural}</option>
-                    <option value="96">96 {itemNamePlural}</option>
+                    <option value={6}>6 {itemNamePlural}</option>
+                    <option value={12}>12 {itemNamePlural}</option>
+                    <option value={24}>24 {itemNamePlural}</option>
+                    <option value={48}>48 {itemNamePlural}</option>
+                    <option value={96}>96 {itemNamePlural}</option>
                     <option value="">All {itemNamePlural}</option>
                 </select>
             </div>

c48618b7e3b53c3dd4b12cd6dc611ade7f522a38
Mike Linden <mike@mikelinden.com>
2023-02-04 14:44:22 -0500

    add datasource column type

--- src/components/render-prop-value/index.js
+++ src/components/render-prop-value/index.js
@@ -1,14 +1,27 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import { getCurrentModuleConfig } from 'redux/selectors/get-current-module-config';
 
 import RenderPropValue from './render-prop-value';
+import { fetchDatasource } from 'redux/actions/data-sources';
+import getModuleReduxProp from 'utils/get-module-redux-prop';
 
 function mapStateToProps(state, ownProps) {
     return {
         ...ownProps,
-        config: getCurrentModuleConfig(state, ownProps)
+        config: getCurrentModuleConfig(state, ownProps),
+        datasources: getModuleReduxProp(state, 'datasources'),
     };
 }
 
-export default connect(mapStateToProps)(RenderPropValue);
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({
+            fetchDatasource
+        }, dispatch)
+    };
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(RenderPropValue);
